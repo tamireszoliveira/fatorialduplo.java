c432c5fceb5463ed4ae574b36446901d5ed1fdd4
Tami Oliveira <tamiresaldeoliveira@gmail.com>
2025-02-18 11:11:57 -0300

    mudança enunciado

--- src/view/Mainfatduplo.java
+++ src/view/Mainfatduplo.java
@@ -3,7 +3,10 @@ package view;
 import javax.swing.JOptionPane;
 
 import controller.FatduploController;
-
+/* 4. Faça uma função recursiva que receba um número inteiro posi�vo ímpar N e retorne o fatorial duplo
+desse número (A entrada deve ser validada fora da função recursiva). O fatorial duplo é definido como o
+produto de todos os números naturais ímpares de 1 até algum número natural ímpar N. Assim, o fatorial
+duplo de 5 é: */
 
 public class Mainfatduplo {
     public static void main(String[] args) {
